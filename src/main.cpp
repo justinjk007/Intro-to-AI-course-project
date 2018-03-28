fc0285c6eb9f747fa44bb7a760a0987e20d7c764
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-28 18:11:15 -0400

    More test points

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,21 @@ int main()
     Point<int> testPoint3(400, 800);
     Point<int> testPoint4(500, 660);
     Point<int> testPoint5(0, 100);
-
+    Point<int> testPoint6(200, 100);
+    Point<int> testPoint7(23, 23);
+    Point<int> testPoint8(56, 35);
+    Point<int> testPoint9(99, 10);
+    Point<int> testPoint10(17, 10);
     world.renderAgent(testPoint1, 0);
     world.renderAgent(testPoint2, 1);
-    world.renderTarget(testPoint3, 0);
-    world.renderTarget(testPoint4, 1);
-    world.renderTarget(testPoint5, 4);
+    world.renderAgent(testPoint3, 2);
+    world.renderAgent(testPoint4, 3);
+    world.renderAgent(testPoint5, 4);
+    world.renderAgent(testPoint6, 0);
+    world.renderTarget(testPoint7, 1);
+    world.renderTarget(testPoint8, 2);
+    world.renderTarget(testPoint9, 3);
+    world.renderTarget(testPoint10, 4);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(10000));
     return 0;
